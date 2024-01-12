bbc3b1b0ed0fb8c9e846f155dfffcd78c6ee6621
blaine-t <blaine-t@bathost.net>
2024-01-12 16:32:42 -0600

    Implement repeat URI back to user

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,7 @@ use http_body_util::Empty;
 use http_body_util::Full;
 use http_body_util::{combinators::BoxBody, BodyExt};
 
-use hyper::body::Body;
 use hyper::body::Bytes;
-use hyper::body::Frame;
 use hyper::server::conn::http1;
 use hyper::service::service_fn;
 use hyper::{Method, StatusCode};
@@ -16,53 +14,15 @@ use hyper_util::rt::TokioIo;
 
 use tokio::net::TcpListener;
 
-async fn echo(
+async fn handle_request(
     req: Request<hyper::body::Incoming>,
 ) -> Result<Response<BoxBody<Bytes, hyper::Error>>, hyper::Error> {
-    match (req.method(), req.uri().path()) {
-        (&Method::GET, "/") => Ok(Response::new(full("Try POSTing data to /echo"))),
-        (&Method::POST, "/echo") => Ok(Response::new(req.into_body().boxed())),
-        (&Method::POST, "/echo/uppercase") => {
-            // Map this body's frame to a different type
-            let frame_stream = req.into_body().map_frame(|frame| {
-                let frame = if let Ok(data) = frame.into_data() {
-                    // Convert every byte in every Data frame to uppercase
-                    data.iter()
-                        .map(|byte| byte.to_ascii_uppercase())
-                        .collect::<Bytes>()
-                } else {
-                    Bytes::new()
-                };
-
-                Frame::data(frame)
-            });
-
-            Ok(Response::new(frame_stream.boxed()))
-        }
-        (&Method::POST, "/echo/reversed") => {
-            // Protect our server from massive bodies.
-            let upper = req.body().size_hint().upper().unwrap_or(u64::MAX);
-            if upper > 1024 * 64 {
-                let mut resp = Response::new(full("Body too big"));
-                *resp.status_mut() = hyper::StatusCode::PAYLOAD_TOO_LARGE;
-                return Ok(resp);
-            }
-
-            // Await the whole body to be collected into a single `Bytes`...
-            let whole_body = req.collect().await?.to_bytes();
-
-            // Iterate the whole body in reverse order and collect into a new Vec.
-            let reversed_body = whole_body.iter().rev().cloned().collect::<Vec<u8>>();
-
-            Ok(Response::new(full(reversed_body)))
-        }
-        // Return 404 Not Found for other routes.
-        _ => {
-            let mut not_found = Response::new(empty());
-            *not_found.status_mut() = StatusCode::NOT_FOUND;
-            Ok(not_found)
-        }
+    if req.method() == Method::GET {
+        return Ok(Response::new(full(req.uri().path().to_owned())));
     }
+    let mut not_found = Response::new(empty());
+    *not_found.status_mut() = StatusCode::NOT_FOUND;
+    Ok(not_found)
 }
 
 // We create some utility functions to make Empty and Full bodies
@@ -98,7 +58,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
             // Finally, we bind the incoming connection to our `echo` service
             if let Err(err) = http1::Builder::new()
                 // `service_fn` converts our function in a `Service`
-                .serve_connection(io, service_fn(echo))
+                .serve_connection(io, service_fn(handle_request))
                 .await
             {
                 println!("Error serving connection: {:?}", err);
